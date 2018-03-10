1a395eac331ce9dac88e3736d4869ee10cb95900
@marciosmelo <marciomelo@gmail.com>
2018-03-09 21:40:38 -0300

    Adding the Boom Dots starter project

---
+++ boomdots/src/scenes/preloader.js
@@ -0,0 +1,13 @@
+import phaser from 'phaser'
+export class Preloader extends phaser.Scene {
+    constructor () {
+        super({
+            key: 'preloader'
+        })
+    }
+    preload () {
+        this.load.image('bg-static', 'assets/square.png')
+        this.load.image('bg-overlay', 'assets/bg.png')
+        console.log('Preloader preload')
+    }
+}
